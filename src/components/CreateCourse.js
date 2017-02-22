12827dc26e7476bdc069653ded99533916721397
Rishabh Mishra <mishrarishabh865@gmail.com>
2017-02-22 15:54:33 +0530

    Removed bundle.js Added webpack script in package.json changed button pointer

--- src/components/CreateCourse.js
+++ src/components/CreateCourse.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import {ipcRenderer} from 'electron'
-import {hashHistory} from 'react-router'
+import { ipcRenderer } from 'electron'
+import { hashHistory } from 'react-router'
 
- ipcRenderer.on('add-course',(event, arg)=>{
-        console.log("event,arg",arg)
+ipcRenderer.on('add-course', (event, arg) => {
+  console.log('event,arg', arg)
 })
 export default class CreateCourse extends Component {
   componentWillMount () {
@@ -14,30 +14,30 @@ export default class CreateCourse extends Component {
       nameClass: 'inputText',
       dateClass: 'inputText',
       durationClass: 'inputText',
-      submitted : false
+      submitted: false
     })
   }
-  sendData(){
+  sendData () {
     let obj = {
-        name : this.state.name,
-        duration : this.state.duration,
-        startDate : this.state.startdate
+      name: this.state.name,
+      duration: this.state.duration,
+      startDate: this.state.startdate
     }
-    ipcRenderer.send('add-course',obj)
+    ipcRenderer.send('add-course', obj)
     this.setState({
-        submitted : true
+      submitted: true
     })
   }
-   
+
   handleSubmit () {
-    if (this.state.name === '' || this.state.startdate === '' || this.state.duration === null || this.state.duration <= 0 ) {
+    if (this.state.name === '' || this.state.startdate === '' || this.state.duration === null || this.state.duration <= 0) {
       if (this.state.name === '') {
         this.setState({nameClass: 'inputText errorInput'})
       }
       if (this.state.startdate === '') {
         this.setState({dateClass: 'inputText errorInput'})
       }
-      if (this.state.duration === null || this.state.duration <= 0 ) {
+      if (this.state.duration === null || this.state.duration <= 0) {
         this.setState({durationClass: 'inputText errorInput'})
       }
     }else {
@@ -45,25 +45,26 @@ export default class CreateCourse extends Component {
     }
   }
 
-  renderLoading(){
-      return(
-          <div className="loaderWrapper">
-              <div className="loader"></div>
-
-              </div>
-      )
+  renderLoading () {
+    return (
+      <div className='loaderWrapper'>
+        <div className='loader'></div>
+      </div>
+    )
   }
 
-  renderForm(){
-      return(<div className='createCourseForm'>
-            <h1>Start Course</h1>
-              <div>
+  renderForm () {
+    return (<div>
+              <div className='header'>
+                <button className='buttons nav-buttons' onClick={() => hashHistory.push('/')}>
+                  Home
+                </button>
+              </div>
+              <div className='createCourseForm'>
+                <h1>Start Course</h1>
                 <div className='formField'>
                   <span className='formLabel'>Course Name</span>
-                  <input
-                    type='text'
-                    className={this.state.nameClass}
-                    onChange={e => this.setState({name: e.target.value.trim(), nameClass: 'inputText'})} />
+                  <input type='text' className={this.state.nameClass} onChange={e => this.setState({name: e.target.value.trim(), nameClass: 'inputText'})} />
                 </div>
                 <div className='formField'>
                   <span className='formLabel'>Start Date</span>
@@ -71,22 +72,23 @@ export default class CreateCourse extends Component {
                 </div>
                 <div className='formField'>
                   <span className='formLabel'>Duration</span>
-                  <input type='number' min="1" className={this.state.durationClass} onChange={e => this.setState({duration: Number(e.target.value), durationClass: 'inputText'})} />
+                  <input
+                    type='number'
+                    min='1'
+                    className={this.state.durationClass}
+                    onChange={e => this.setState({duration: Number(e.target.value), durationClass: 'inputText'})} />
                 </div>
                 <button className='submitButton buttons' onClick={this.handleSubmit.bind(this)}>
                   Submit
                 </button>
               </div>
-              </div>
-      )
+            </div>
+    )
   }
 
   render () {
     return (<div className='container'>
-            <div className="header">
-                <button className="buttons nav-buttons" onClick={() => hashHistory.push('/')} >Home </button>
-            </div>
-            {this.state.submitted ? this.renderLoading() : this.renderForm()}
+              {this.state.submitted ? this.renderLoading() : this.renderForm()}
             </div>)
   }
 }
