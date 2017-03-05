e5606e708aa949ddba358fa4f0b5d0bbd27feec9
Rishabh Mishra <mishrarishabh865@gmail.com>
2017-03-05 20:22:43 +0530

    Created Table for Student Transactions

--- src/components/ViewStudent.js
+++ src/components/ViewStudent.js
@@ -29,7 +29,88 @@ export default class ViewStudent extends Component {
                     </div>
                  </div>
                  <div className="transactionsTable">
-                     <h1>demo </h1>
+                     <div className='tableHeader'>
+                        <div className='tableColumn dateColumn'>
+                        Date
+                        </div>
+                        <div className='tableColumn typeColumn'>
+                        Type
+                        </div>
+                        <div className='tableColumn nameColumn'>
+                        Name
+                        </div>
+                        <div className='tableColumn rateColumn'>
+                        Rate
+                        </div>
+                        <div className='tableColumn quantityColumn'>
+                        Quantity
+                        </div>
+                        <div className='tableColumn amountColumn'>
+                        Amount
+                        </div>
+                    </div>
+                    <div className="txnTableBody">
+                        <div className="tableRow">
+                            <div className='txntableColumn dateColumn'>
+                            12/01/2017
+                            </div>
+                            <div className='txntableColumn typeColumn'>
+                            Purchase
+                            </div>
+                            <div className='txntableColumn nameColumn'>
+                            Tooth Paste
+                            </div>
+                            <div className='txntableColumn rateColumn'>
+                            50
+                            </div>
+                            <div className='txntableColumn quantityColumn'>
+                            20
+                            </div>
+                            <div className='txntableColumn amountColumn'>
+                            1000
+                            </div>
+                        </div>
+                        <div className="tableRow">
+                            <div className='txntableColumn dateColumn'>
+                            15/01/2017
+                            </div>
+                            <div className='txntableColumn typeColumn'>
+                            Deposit
+                            </div>
+                            <div className='txntableColumn nameColumn'>
+                            -
+                            </div>
+                            <div className='txntableColumn rateColumn'>
+                            -
+                            </div>
+                            <div className='txntableColumn quantityColumn'>
+                            -
+                            </div>
+                            <div className='txntableColumn amountColumn'>
+                            2000
+                            </div>
+                        </div>
+                        <div className="tableRow">
+                            <div className='txntableColumn dateColumn'>
+                            09/03/2017
+                            </div>
+                            <div className='txntableColumn typeColumn'>
+                            Laundry
+                            </div>
+                            <div className='txntableColumn nameColumn'>
+                            -
+                            </div>
+                            <div className='txntableColumn rateColumn'>
+                            20
+                            </div>
+                            <div className='txntableColumn quantityColumn'>
+                            10
+                            </div>
+                            <div className='txntableColumn amountColumn'>
+                            200
+                            </div>
+                        </div>
+                    </div>
                  </div>
                  <div className='footer'>
                     <button className='buttons footer-buttons' onClick={() => hashHistory.push(`/adddeposit/${this.props.params.cid}/${this.props.params.sid}`)}>
